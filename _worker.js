d7f672abb40813a2d363e7ef149d5ec88944867f
vpnshare863 <vpnshare863@gmail.com>
2024-07-11 04:22:10 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e66c2815-f1df-4be4-a2a9-c5a20d3cf711';
 
-const พร็อกซีไอพีs = ['165.22.250.249'];
+const พร็อกซีไอพีs = ['152.42.214.236'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
